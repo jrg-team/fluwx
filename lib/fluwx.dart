3fea94c539d8fd13314d28da0f261da410a04583
Big Lion <biglion77@outlook.com>
2021-12-16 10:57:24 +0800

    update name

---
+++ lib/fluwx.dart
@@ -0,0 +1,11 @@
+/// Fluwx is a powerful plugin for WeChatSDK.
+/// easy to use.
+///
+///  A open sou;rce project authorized by [OpenFlutter](https://github.com/OpenFlutter).
+library fluwx;
+
+export 'src/fluwx_iml.dart';
+export 'src/response/wechat_response.dart';
+export 'src/share/share_models.dart';
+export 'src/wechat_enums.dart';
+export 'src/wechat_file.dart' hide FileSchema;
